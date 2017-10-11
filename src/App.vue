2e2038476cb8b76d66a29a76b4c0cbcf4ade2c31
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2017-10-11 19:07:42 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ToDo/>
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo'
+
+export default {
+  name: 'app',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <span>SERVING USER:</span>
+      <span>{{ id_user }}</span>
+    </div>
+    <div>
+      <div>TASKS</div>
+      <div id="task-container"></div>
+      <div>
+        <input type="button" value="+">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ToDo',
+    data () {
+      return {
+        id_user: 'NOT YET IMPLMENTED'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
