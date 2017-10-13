5f4730773b182505b1541240c896422e87a66a72
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2017-10-13 16:52:28 -0400

    Added dependencies

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -79,10 +79,14 @@
     transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
   }
 
-  .slide-fade-enter, .slide-fade-leave-to
+  .slide-fade-enter {
     /* .slide-fade-leave-active below version 2.1.8 */
-  {
-    transform: translateX(10px);
+    transform: translateY(10px);
+    opacity: 0;
+  }
+
+  .slide-fade-leave-to {
+    transform: translateY(-10px);
     opacity: 0;
   }
 </style>
